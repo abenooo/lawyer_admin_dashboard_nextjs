1b0152598ab8cab3878ef047de6cc8d6b6379398
Abenezer <abenezerkifle000@gmail.com>
2024-04-04 09:23:51 -0700

    Clean and render sidebar,navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,36 @@
-import Image from "next/image";
+import { Karla } from 'next/font/google'
+import './globals.css'
+import Header from '@/components/header';
+import PageWrapper from '@/components/pagewrapper';
+import { Metadata } from 'next';
+import SideBar from '@/components/SideBar';
 
-export default function Home() {
+const karla = Karla({
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
+  subsets: ['latin'],
+  variable: "--font-karla"
+})
+export const metadata: Metadata = {
+  title: "lawyer dashboard",
+  description: "NextJs admin dashboard created by Sijin Raj"
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
-   <>
-   <h1>Dashboard</h1>
-   </>
-  );
+    <html lang="en" suppressHydrationWarning>
+      <body className={karla.className + ' h-screen overflow-hidden'}>
+       
+          <>
+            <SideBar></SideBar>
+              <Header></Header>
+              <PageWrapper>  </PageWrapper>
+              
+          </>
+      </body>
+    </html>
+  )
 }
