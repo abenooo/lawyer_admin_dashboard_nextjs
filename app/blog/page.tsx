5a7e0e15cd0a9e0a87ea5a3e55d81ac34b585ef5
Abenezer <abenezerkifle000@gmail.com>
2024-04-04 23:56:46 -0700

    Create blog page

---
+++ app/blog/page.tsx
@@ -0,0 +1,10 @@
+
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+
+export default function Home() {
+  return (
+    <>
+       <h2 className="text-3xl font-bold tracking-tight my-4">Blog</h2>
+    </>
+  )
+}
