e8d721f36e9e7eb160ff11f1c44163fd9eab55f1
Abenezer <abenezerkifle000@gmail.com>
2024-04-08 12:35:46 -0700

    Update profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -119,39 +119,7 @@ export default function page() {
             </FormItem>
           )}
         />
-        <div>
-          {fields.map((field, index) => (
-            <FormField
-              control={form.control}
-              key={field.id}
-              name={`urls.${index}.value`}
-              render={({ field }: { field: ControllerRenderProps<ProfileFormValues, `urls.${number}.value`> }) => (
-                <FormItem>
-                  <FormLabel className={cn(index !== 0 && "sr-only")}>
-                    URLs
-                  </FormLabel>
-                  <FormDescription className={cn(index !== 0 && "sr-only")}>
-                    Add links to your website, blog, or social media profiles.
-                  </FormDescription>
-                  <FormControl>
-                    <Input {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          ))}
-          {/* <Button
-            type="button"
-            variant="outline"
-            size="sm"
-            className="mt-2"
-            onClick={() => append({ value: "" })}
-          >
-            Add URL
-          </Button> */}
-        </div>
-        <button type="submit">Update profile</button>
+        <Button type="submit">Update profile</Button>
       </form>
     </Form>
   )
