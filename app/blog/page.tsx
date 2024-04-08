133be7b0d1d1d77b8fd77e542b239b6007855891
Abenezer <abenezerkifle000@gmail.com>
2024-04-08 01:35:52 -0700

    Display dynamic data from json server

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -35,157 +35,104 @@ interface Recipe {
   time: number;
   description: string;
   vegan: boolean;
+  date: string;
   id: string;
 }
-
-const page = () => {
+// async function to get the list of data
+async function getRecipes(): Promise<Recipe[]> {
+  const result = await fetch("http://localhost:4000/recipes");
+  return result.json();
+}
+async function page() {
+  const recipes = await getRecipes();
   return (
-    <Table>
-      <TableCaption>A list of your recent news.</TableCaption>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-[100px]">Number</TableHead>
-          <TableHead>Status</TableHead>
-          <TableHead>Title</TableHead>
-          <TableHead>Description</TableHead>
-          <TableHead>Date</TableHead>
-          <TableHead>Image</TableHead>
-          <TableHead className="text-right">Operation</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody className="border-b border-natural-200 transition duration-300 ease-in-out">
-        <TableRow>
-          <TableCell className="font-medium">1</TableCell>
-          <TableCell>Pending</TableCell>
-          <TableCell>Civil low</TableCell>
-          <TableCell>Talks about civil low in this blog</TableCell>
-          <TableCell>01/02/2024</TableCell>
-          <TableCell>
-            <Avatar>
-              <AvatarImage src="https://github.com/shadcn.png" />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-          </TableCell>
-          <TableCell className="text-right">
-            <AlertDialog>
-              <AlertDialogTrigger className="text-red-500">
-                Delete
-              </AlertDialogTrigger>
-              <AlertDialogContent>
-                <AlertDialogHeader>
-                  <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-                  <AlertDialogDescription>
-                    This action cannot be undone. This will permanently delete
-                    your account and remove your data from our servers.
-                  </AlertDialogDescription>
-                </AlertDialogHeader>
-                <AlertDialogFooter>
-                  <AlertDialogCancel>Cancel</AlertDialogCancel>
-                  <AlertDialogAction>Continue</AlertDialogAction>
-                </AlertDialogFooter>
-              </AlertDialogContent>
-            </AlertDialog>
-          </TableCell>
-          <TableCell>View</TableCell>
-          <TableCell>Edit</TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell className="font-medium">2</TableCell>
-          <TableCell>Done</TableCell>
-          <TableCell>Civil low</TableCell>
-          <TableCell>Talks about civil low in this blog</TableCell>
-          <TableCell>01/02/2024</TableCell>
-          <TableCell>
-            <Avatar>
-              <AvatarImage src="https://github.com/shadcn.png" />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-          </TableCell>
-          <TableCell className="text-right">
-            <AlertDialog>
-              <AlertDialogTrigger className="text-red-500">
-                Delete
-              </AlertDialogTrigger>
-              <AlertDialogContent>
-                <AlertDialogHeader>
-                  <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-                  <AlertDialogDescription>
-                    This action cannot be undone. This will permanently delete
-                    your account and remove your data from our servers.
-                  </AlertDialogDescription>
-                </AlertDialogHeader>
-                <AlertDialogFooter>
-                  <AlertDialogCancel>Cancel</AlertDialogCancel>
-                  <AlertDialogAction>Continue</AlertDialogAction>
-                </AlertDialogFooter>
-              </AlertDialogContent>
-            </AlertDialog>
-          </TableCell>
-          <TableCell>View</TableCell>
-          <TableCell>Edit</TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell className="font-medium">3</TableCell>
-          <TableCell>Done</TableCell>
-          <TableCell>Civil low</TableCell>
-          <TableCell>Talks about civil low in this blog</TableCell>
-          <TableCell>01/02/2024</TableCell>
-          <TableCell>
-            <Avatar>
-              <AvatarImage src="https://github.com/shadcn.png" />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-          </TableCell>
-          <TableCell className="text-right">
-            <AlertDialog>
-              <AlertDialogTrigger className="text-red-500">
-                Delete
-              </AlertDialogTrigger>
-              <AlertDialogContent>
-                <AlertDialogHeader>
-                  <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-                  <AlertDialogDescription>
-                    This action cannot be undone. This will permanently delete
-                    your account and remove your data from our servers.
-                  </AlertDialogDescription>
-                </AlertDialogHeader>
-                <AlertDialogFooter>
-                  <AlertDialogCancel>Cancel</AlertDialogCancel>
-                  <AlertDialogAction>Continue</AlertDialogAction>
-                </AlertDialogFooter>
-              </AlertDialogContent>
-            </AlertDialog>
-          </TableCell>
-          <TableCell>View</TableCell>
-          <TableCell>Edit</TableCell>
-        </TableRow>
-      </TableBody>
-      <Pagination>
-        <PaginationContent className="flex justify-start md:justify-end">
-          <PaginationItem>
-            <PaginationPrevious href="#" />
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationLink href="#">1</PaginationLink>
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationLink href="#" isActive>
-              2
-            </PaginationLink>
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationLink href="#">3</PaginationLink>
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationEllipsis />
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationNext href="#" />
-          </PaginationItem>
-        </PaginationContent>
-      </Pagination>
-    </Table>
+    <>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Number</TableHead>
+            <TableHead>Status</TableHead>
+            <TableHead>Title</TableHead>
+            <TableHead>Description</TableHead>
+            <TableHead>Date</TableHead>
+            <TableHead>Image</TableHead>
+            <TableHead className="text-right">Operation</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {recipes.map((recipe) => (
+            <TableRow key={recipe.id}>
+              <TableCell>{recipe.id}</TableCell>
+              <TableCell>{recipe.title}</TableCell>
+              <TableCell>{recipe.title}</TableCell>
+              <TableCell>{recipe.description}</TableCell>
+              <TableCell>{recipe.date}</TableCell>
+              <TableCell>
+                <Avatar>
+                  <AvatarImage src="https://github.com/shadcn.png" />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+              </TableCell>
+              <TableCell>
+                <TableCell className="text-right">
+                  <AlertDialog>
+                    <AlertDialogTrigger className="text-red-500">
+                      Delete
+                    </AlertDialogTrigger>
+                    <AlertDialogContent>
+                      <AlertDialogHeader>
+                        <AlertDialogTitle>
+                          Are you absolutely sure?
+                        </AlertDialogTitle>
+                        <AlertDialogDescription>
+                          This action cannot be undone. This will permanently
+                          delete your account and remove your data from our
+                          servers.
+                        </AlertDialogDescription>
+                      </AlertDialogHeader>
+                      <AlertDialogFooter>
+                        <AlertDialogCancel>Cancel</AlertDialogCancel>
+                        <AlertDialogAction>Continue</AlertDialogAction>
+                      </AlertDialogFooter>
+                    </AlertDialogContent>
+                  </AlertDialog>
+                </TableCell>
+                <TableCell>View</TableCell>
+                <TableCell>Edit</TableCell>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+
+      <Table>
+        <Pagination>
+          <PaginationContent className="flex justify-start md:justify-end">
+            <PaginationItem>
+              <PaginationPrevious href="#" />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink href="#">1</PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink href="#" isActive>
+                2
+              </PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink href="#">3</PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationEllipsis />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationNext href="#" />
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
+      </Table>
+    </>
   );
-};
+}
 
 export default page;
