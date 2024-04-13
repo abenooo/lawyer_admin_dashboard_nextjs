9921008799a0bc4e2625218f367150ebcb66f582
Abenezer <abenezerkifle000@gmail.com>
2024-04-13 11:55:04 -0700

    Change the fake data to dynamic data from api

--- app/(dashboard)/news/page.tsx
+++ app/(dashboard)/news/page.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client"
+import React, { useEffect, useState } from "react";
 import {
   Table,
   TableBody,
@@ -29,111 +30,91 @@ import {
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-interface Recipe {
-  title: string;
+
+interface Blog {
+  _id: string;
+  NewsTitle: string;
+  NewsCategory: string;
+  NewsDescription: string;
   image: string;
-  time: number;
-  description: string;
-  vegan: boolean;
-  date: string;
-  id: string;
-  status:string;
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
 }
-// async function to get the list of data
-async function getRecipes(): Promise<Recipe[]> {
-  const result = await fetch("http://localhost:4000/recipes");
-  return result.json();
-}
-async function page() {
-  const recipes = await getRecipes();
+
+const fetchBlogs = async (): Promise<Blog[]> => {
+  try {
+    const response = await fetch("https://lawyerpw.onrender.com/api/news");
+    if (!response.ok) {
+      throw new Error('Failed to fetch data');
+    }
+    return response.json();
+  } catch (error) {
+    console.error("Error fetching blogs:", error);
+    return [];
+  }
+};
+
+const page: React.FC = () => {
+  const [blogs, setBlogs] = useState<Blog[]>([]);
+
+  useEffect(() => {
+    fetchBlogs().then(setBlogs);
+  }, []);
+
   return (
     <>
       <Table>
         <TableHeader>
           <TableRow>
-            <TableHead className="w-[100px]">Number</TableHead>
-            <TableHead>Status</TableHead>
+            <TableHead className="w-[100px]">ID</TableHead>
             <TableHead>Title</TableHead>
             <TableHead>Description</TableHead>
+            <TableHead>Category</TableHead>
             <TableHead>Date</TableHead>
             <TableHead>Image</TableHead>
-            <TableHead className="text-right">Operation</TableHead>
+            <TableHead className="text-right">Operations</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
-          {recipes.map((recipe) => (
-            <TableRow key={recipe.id}>
-              <TableCell>{recipe.id}</TableCell>
-              <TableCell>{recipe.status}</TableCell>
-              <TableCell>{recipe.title}</TableCell>
-              <TableCell>{recipe.description}</TableCell>
-              <TableCell>{recipe.date}</TableCell>
-              <TableCell>
+          {blogs.map((blog, index) => (
+            <TableRow key={blog._id}>
+              <TableCell>{index + 1}</TableCell>
+              <TableCell>{blog.NewsTitle}</TableCell>
+              <TableCell>{blog.NewsDescription}</TableCell>
+              <TableCell>{blog.NewsCategory}</TableCell>
+              <TableCell>{new Date(blog.createdAt).toLocaleDateString()}</TableCell>
+              {/* <TableCell>
                 <Avatar>
-                  <AvatarImage src="https://github.com/shadcn.png" />
-                  <AvatarFallback>CN</AvatarFallback>
+                  <AvatarImage src={blog.image} />
+                  <AvatarFallback>{blog.BlogTitle[0]}</AvatarFallback>
                 </Avatar>
-              </TableCell>
-              <TableCell>
-                <TableCell className="text-right">
-                  <AlertDialog>
-                    <AlertDialogTrigger className="text-red-500">
-                      Delete
-                    </AlertDialogTrigger>
-                    <AlertDialogContent>
-                      <AlertDialogHeader>
-                        <AlertDialogTitle>
-                          Are you absolutely sure?
-                        </AlertDialogTitle>
-                        <AlertDialogDescription>
-                          This action cannot be undone. This will permanently
-                          delete your account and remove your data from our
-                          servers.
-                        </AlertDialogDescription>
-                      </AlertDialogHeader>
-                      <AlertDialogFooter>
-                        <AlertDialogCancel>Cancel</AlertDialogCancel>
-                        <AlertDialogAction>Continue</AlertDialogAction>
-                      </AlertDialogFooter>
-                    </AlertDialogContent>
-                  </AlertDialog>
-                </TableCell>
-                <TableCell>View</TableCell>
-                <TableCell>Edit</TableCell>
-              </TableCell>
+              </TableCell> */}
+              {/* <TableCell className="text-right">
+                <AlertDialogTrigger className="text-red-500">
+                  Delete
+                </AlertDialogTrigger>
+                <AlertDialog>
+                  <AlertDialogContent>
+                    <AlertDialogHeader>
+                      <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                      <AlertDialogDescription>
+                        This action cannot be undone. This will permanently delete the blog post.
+                      </AlertDialogDescription>
+                    </AlertDialogHeader>
+                    <AlertDialogFooter>
+                      <AlertDialogCancel>Cancel</AlertDialogCancel>
+                      <AlertDialogAction>Continue</AlertDialogAction>
+                    </AlertDialogFooter>
+                  </AlertDialogContent>
+                </AlertDialog>
+              </TableCell> */}
             </TableRow>
           ))}
         </TableBody>
       </Table>
-
-      <Table>
-        <Pagination>
-          <PaginationContent className="flex justify-start md:justify-end">
-            <PaginationItem>
-              <PaginationPrevious href="#" />
-            </PaginationItem>
-            <PaginationItem>
-              <PaginationLink href="#">1</PaginationLink>
-            </PaginationItem>
-            <PaginationItem>
-              <PaginationLink href="#" isActive>
-                2
-              </PaginationLink>
-            </PaginationItem>
-            <PaginationItem>
-              <PaginationLink href="#">3</PaginationLink>
-            </PaginationItem>
-            <PaginationItem>
-              <PaginationEllipsis />
-            </PaginationItem>
-            <PaginationItem>
-              <PaginationNext href="#" />
-            </PaginationItem>
-          </PaginationContent>
-        </Pagination>
-      </Table>
     </>
   );
-}
+};
 
 export default page;
