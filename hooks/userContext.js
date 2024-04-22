e512395ed519498ff27c45c1c43bc17793db280d
Abenezer <abenezerkifle000@gmail.com>
2024-04-22 10:57:05 -0700

    Add content for user loggin

---
+++ hooks/userContext.js
@@ -0,0 +1,33 @@
+"use client"; // This is necessary for Client Components in Next.js 13
+
+import { createContext, useContext, useEffect, useState } from "react";
+
+const UserContext = createContext(null);
+
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    // Load the user from localStorage on initial load
+    const storedUser = localStorage.getItem('user');
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+  }, []);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUser = () => {
+  const context = useContext(UserContext);
+
+  if (context === null) {
+    throw new Error('useUser must be used within a UserProvider');
+  }
+
+  return context;
+};
